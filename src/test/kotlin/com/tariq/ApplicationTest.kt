0044d677ea0389aa7835aad881d36f19a33cc347
tariq2518 <tariq.hussain2518@gmail.com>
2023-06-04 00:51:00 +0500

    implementation of test cases Testing endpoints

--- src/test/kotlin/com/tariq/ApplicationTest.kt
+++ src/test/kotlin/com/tariq/ApplicationTest.kt
@@ -1,21 +1,139 @@
 package com.tariq
 
+import com.tariq.models.AnimeApiResponse
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.server.testing.*
 import kotlin.test.*
 import io.ktor.http.*
 import com.tariq.plugins.*
+import com.tariq.repository.AnimeHeroesRepository
+import com.tariq.repository.AnimeHeroesRepositoryImp
+import kotlinx.serialization.json.Json
+import org.koin.java.KoinJavaComponent.inject
 
 class ApplicationTest {
-//    @Test
-//    fun testRoot() = testApplication {
-//        application {
-//            configureRouting()
-//        }
-//        client.get("/").apply {
-//            assertEquals(HttpStatusCode.OK, status)
-//            assertEquals("Hello World!", bodyAsText())
-//        }
-//    }
+
+    private val animeHeroesRepository: AnimeHeroesRepository by inject(AnimeHeroesRepository::class.java)
+
+    @Test
+    fun `root end points, correct information`() = testApplication {
+        application {
+            configureRouting()
+        }
+        client.get("/").apply {
+            assertEquals(
+                expected = HttpStatusCode.OK,
+                actual = status
+            )
+            assertEquals(
+                expected = "API for the Anime Heroes",
+                actual = bodyAsText()
+            )
+        }
+    }
+
+
+    @Test
+    fun `anime heroes endpoint, correct information`() = testApplication {
+        environment {
+            developmentMode = false
+        }
+        application {
+            configureRouting()
+        }
+
+        client.get("/anime/heroes").apply {
+            assertEquals(
+                expected = HttpStatusCode.OK,
+                actual = status
+            )
+
+            val expectedOutput = AnimeApiResponse(
+                success = true,
+                message = "Here are Heroes",
+                previousPage = null,
+                nextPage = 2,
+                animeHeroes = animeHeroesRepository.page1
+            )
+
+            val actualOutput = Json.decodeFromString<AnimeApiResponse>(bodyAsText())
+
+            assertEquals(
+                expected = expectedOutput,
+                actual = actualOutput
+            )
+
+        }
+    }
+
+
+    @Test
+    fun `anime heroes endpoint all pages, correct information`() = testApplication {
+        environment {
+            developmentMode = false
+        }
+        application {
+            configureRouting()
+        }
+
+        val pages = 1..6
+//        val listOfAllHeroes = listOf(
+//            animeHeroesRepository.page1,
+//            animeHeroesRepository.page2,
+//            animeHeroesRepository.page3,
+//            animeHeroesRepository.page4,
+//            animeHeroesRepository.page5,
+//            animeHeroesRepository.page6,
+//        )
+
+
+        pages.forEach{
+            client.get("/anime/heroes?page=$it").apply {
+                assertEquals(
+                    expected = HttpStatusCode.OK,
+                    actual = status
+                )
+
+                val expectedOutput = AnimeApiResponse(
+                    success = true,
+                    message = "Here are Heroes",
+                    previousPage = getCurrentPage(page = it)["previousPage"],
+                    nextPage = getCurrentPage(page = it)["nextPage"],
+                    animeHeroes = listOf(animeHeroesRepository.page1, animeHeroesRepository.page2, animeHeroesRepository.page3,
+                        animeHeroesRepository.page4, animeHeroesRepository.page5, animeHeroesRepository.page6)[it -1]
+                )
+
+                val actualOutput = Json.decodeFromString<AnimeApiResponse>(bodyAsText())
+
+                assertEquals(
+                    expected = expectedOutput,
+                    actual = actualOutput
+                )
+
+            }
+        }
+    }
+
+    private fun getCurrentPage(page: Int): Map<String, Int?> {
+        var previousPage: Int? = page
+        var nextPage: Int? = page
+
+        if (page in 1..5) {
+            nextPage = nextPage?.plus(1)
+        }
+        if (page in 2..6) {
+            previousPage = previousPage?.minus(1)
+        }
+        if (page == 1) {
+            previousPage = null
+        }
+        if (page == 6) {
+            nextPage = null
+        }
+
+        return mapOf("previousPage" to previousPage, "nextPage" to nextPage)
+
+    }
+
 }
